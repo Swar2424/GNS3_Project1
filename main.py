ef989c3b6e016102014aec26cc210d2d358eeb7f
GaspLEV <gaspar.leveque@gmail.com>
2023-12-19 17:28:37 +0100

    ee

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json 
+
+with open('config.json', 'r', encoding='utf-8') as fichier:
+    donnees_lues = json.load(fichier)
+    
 print("AAAA")
 a = 0
 f = open("i1_startup-config.cfg", "w")
