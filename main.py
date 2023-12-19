cf59c682bcf5c0e6d211ce40be2b0750c48498f4
Félix Alonzo <felix.alonzo14@gmail.com>
2023-12-19 19:23:13 +0100

    message

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import json
 with open('config.json', 'r', encoding='utf-8') as fichier:
     donnees_lues = json.load(fichier)
     
-dico = list(donnees_lues.values())[0]
-for AS in dico.values() :
-    for AS1 in AS.values() :
-        print(AS1)
+AS_dic = list(donnees_lues.values())[0]
+
+
+def ASout(address):
+    if address != "NULL" :
+        return (address.split(":")[1] == "2")
+    else :
+        return False
+    
+for AS in AS_dic.values() :
+    for Router in AS["Routeurs"].values() :
+        if ASout(Router['GigabitEthernet 2/0']) :
+            a=0
+        
+        
 
 f = open("i1_startup-config.cfg", "w")
 f.write("!\n! bgp area 11")
