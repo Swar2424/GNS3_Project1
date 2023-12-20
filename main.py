af3dc8d2b9e1cc1a7970e5aca2c9cd7cccdc713f
GaspLEV <gaspar.leveque@gmail.com>
2023-12-20 14:27:21 +0100

    ?

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import json
 with open('config.json', 'r', encoding='utf-8') as fichier:
     donnees_lues = json.load(fichier)
     
+    
 dico = list(donnees_lues.values())[0]
 for AS in dico.values() :
     for AS1 in AS.values() :
         print(AS1)
+
 
 f = open("i1_startup-config.cfg", "w")
 f.write("!\n! bgp area 11")
